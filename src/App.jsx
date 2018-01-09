ddfeb991b947da698d5199054a36ccca3423baeb
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-01-09 17:09:49 +0600

    Providing store

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <div className="title">Reminder Pro</div>
+        <div className="form-inline">
+          <div className="form-group">
+            <input type="text" className="form-control" placeholder="I have to ..."/>
+          </div>
+          <button
+            className="btn btn-success"
+            type="button"
+          >
+            Add Reminder
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;

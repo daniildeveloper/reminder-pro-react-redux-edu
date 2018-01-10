16db3f2981ae81717039c328724fe41c0a5c55bc
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-01-10 12:51:38 +0600

    Intro to actions and action creators

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from "react-redux";
+import {  } from "./";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+
+    }
+  }
+
+  addReminder() {
+    console.log('this.state', this.state); 
+  }
+
   render() {
     return (
       <div className="App container">
         <div className="title">Reminder Pro</div>
         <div className="form-inline">
           <div className="form-group">
-            <input type="text" className="form-control" placeholder="I have to ..."/>
+            <input onChange={event => this.setState({ text: event.target.value })} type="text" className="form-control" placeholder="I have to ..."/>
           </div>
           <button
             className="btn btn-success"
             type="button"
+            onClick={() => this.addReminder()}
           >
             Add Reminder
           </button>
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const ADD_REMINDER = 1;
+export const ADD_REMINDER = 'ADD_REMINDER';
